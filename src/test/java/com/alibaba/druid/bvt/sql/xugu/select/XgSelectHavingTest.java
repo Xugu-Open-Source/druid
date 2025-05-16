54fe6305b4f611f36c56f7a6f01141c3b522c413
WANG Qing <2362072650@qq.com>
2025-05-16 16:37:50 +0800

    add:XuguDB SELECT 单元测试

---
+++ src/test/java/com/alibaba/druid/bvt/sql/xugu/select/XgSelectHavingTest.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 1999-2018 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.alibaba.druid.bvt.sql.xugu.select;
+
+import com.alibaba.druid.sql.SQLUtils;
+import com.alibaba.druid.sql.ast.SQLStatement;
+import com.alibaba.druid.sql.dialect.xugu.parser.XuGuStatementParser;
+import com.alibaba.druid.sql.dialect.xugu.visitor.XuGuSchemaStatVisitor;
+import junit.framework.TestCase;
+
+import java.util.List;
+
+public class XgSelectHavingTest extends TestCase {
+
+    public void testHaving() {
+        String sql = "SELECT region,NAME,SUM(amount) amo_sum,count(*) row_cou FROM TB_TOP GROUP BY region,NAME HAVING row_cou>1;";
+        XuGuStatementParser parser = new XuGuStatementParser(sql);
+        List<SQLStatement> sqlStatements = parser.parseStatementList();
+        SQLStatement stmt = sqlStatements.get(0);
+        XuGuSchemaStatVisitor visitor = new XuGuSchemaStatVisitor();
+        stmt.accept(visitor);
+        System.out.println(SQLUtils.toXuGuString(stmt));
+    }
+}
