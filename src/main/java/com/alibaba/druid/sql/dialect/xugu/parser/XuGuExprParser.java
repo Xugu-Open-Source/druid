797e086f33b3ece2ab57c5e6e71a0a7ebfa81e3e
WANG Qing <2362072650@qq.com>
2026-01-14 14:55:44 +0800

    fix: xugudb 允许 varchar、char 数据类型不指定长度

--- src/main/java/com/alibaba/druid/sql/dialect/xugu/parser/XuGuExprParser.java
+++ src/main/java/com/alibaba/druid/sql/dialect/xugu/parser/XuGuExprParser.java
@@ -448,10 +448,6 @@ public class XuGuExprParser extends SQLExprParser {
                 }
 
                 accept(Token.RPAREN);
-            } else if (lexer.token() == Token.COMMA) {
-                return parseCharTypeRest(charType);
-            } else if (restrict) {
-                accept(Token.LPAREN);
             }
 
             return parseCharTypeRest(charType);
